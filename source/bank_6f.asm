56099725726805d1e25e1a3b95d2d6072cc6534c
Rangi <remy.oukaour+rangi42@gmail.com>
2020-09-11 18:55:32 -0400

    Extract graphics from bank $6E and $6F

--- source/bank_6f.asm
+++ source/bank_6f.asm
@@ -1,4 +1,17 @@
-SECTION "gfx_text_chars", ROMX[$4000], BANK[$6f]
+SECTION "bank6f", ROMX[$4000], BANK[$6f]
+
 gfx_text_chars::
 INCBIN "gfx/text_chars.bin"
+.end::
+
+gfx_lv_2_mt_otsukimi::
+INCBIN "gfx/lv_2_mt_otsukimi.bin"
+.end::
+
+gfx_lv_2_mt_otsukimi_sgb::
+INCBIN "gfx/lv_2_mt_otsukimi_sgb.bin"
+.end::
+
+gfx_lv_2_mt_otsukimi_duplicate::
+INCBIN "gfx/lv_2_mt_otsukimi.bin"
 .end::
