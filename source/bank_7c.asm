f1c173b78fb27a3a0701ea70fe8bd68e60ab9091
Rangi <remy.oukaour+rangi42@gmail.com>
2020-09-14 16:00:18 -0400

    Decompress and extract more "How to Play" graphics

--- source/bank_7c.asm
+++ source/bank_7c.asm
@@ -3,6 +3,11 @@ gfx_alphabets::
 INCBIN "gfx/fonts/alphabets.bin"
 .end::
 
+SECTION "attrmap_how_to_play", ROMX[$5c00], BANK[$7c]
+attrmap_how_to_play::
+INCBIN "gfx/how_to_play/how_to_play.attrmap"
+.end::
+
 SECTION "compressed_tilemap_attrmap_album_pic", ROMX[$7000], BANK[$7c]
 compressed_tilemap_attrmap_album_pic::
 INCBIN "gfx/safari_zone_album/album_pic.tilemap_attrmap.xor"
