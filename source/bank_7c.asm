387bb7be2149599ba5ae7420fe3760e7f5e2656f
Rangi <remy.oukaour+rangi42@gmail.com>
2020-09-14 12:01:11 -0400

    Decompress Safari Zone Album tilemaps/attrmaps

--- source/bank_7c.asm
+++ source/bank_7c.asm
@@ -2,3 +2,13 @@ SECTION "gfx_alphabets", ROMX[$4000], BANK[$7c]
 gfx_alphabets::
 INCBIN "gfx/fonts/alphabets.bin"
 .end::
+
+SECTION "compressed_tilemap_attrmap_album_pic", ROMX[$7000], BANK[$7c]
+compressed_tilemap_attrmap_album_pic::
+INCBIN "gfx/safari_zone_album/album_pic.tilemap_attrmap.xor"
+.end::
+
+SECTION "compressed_tilemap_attrmap_album_list", ROMX[$7482], BANK[$7c]
+compressed_tilemap_attrmap_album_list::
+INCBIN "gfx/safari_zone_album/album_list.tilemap_attrmap.xor"
+.end::
