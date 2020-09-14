c394f413562df274c89538d30b9a6dffc3c59870
Rangi <remy.oukaour+rangi42@gmail.com>
2020-09-14 11:48:41 -0400

    Decompress Pokédex tilemaps/attrmaps

--- source/bank_6c.asm
+++ source/bank_6c.asm
@@ -1,3 +1,8 @@
+SECTION "compressed_tilemap_attrmap_pokedex_pic", ROMX[$7c00], BANK[$6c]
+compressed_tilemap_attrmap_pokedex_pic::
+INCBIN "gfx/pokedex/pokedex_pic.tilemap_attrmap.xor"
+.end::
+
 SECTION "compressed_tilemap_copyright", ROMX[$7eec], BANK[$6c]
 compressed_tilemap_copyright::
 INCBIN "gfx/copyright/copyright.tilemap.xor"
--- source/bank_7a.asm
+++ source/bank_7a.asm
@@ -1,11 +1,14 @@
 SECTION "bank7a", ROMX[$7020], BANK[$7a]
-
 tilemap_title::
 INCBIN "gfx/title/title.tilemap"
 .end::
-
 attrmap_title::
 INCBIN "gfx/title/title.attrmap"
+.end::
+
+SECTION "compressed_tilemap_attrmap_pokedex_list", ROMX[$7820], BANK[$7a]
+compressed_tilemap_attrmap_pokedex_list::
+INCBIN "gfx/pokedex/pokedex_list.tilemap_attrmap.xor"
 .end::
 
 SECTION "compressed_tilemap_attrmap_pokemon_picross", ROMX[$7d18], BANK[$7a]
