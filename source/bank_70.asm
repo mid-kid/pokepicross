545b060733e61fdc5bb14c1ae7bf095757df2793
Rangi <remy.oukaour+rangi42@gmail.com>
2020-09-14 16:13:38 -0400

    Extract unused Picross 2 leftover graphics

--- source/bank_70.asm
+++ source/bank_70.asm
@@ -1,3 +1,8 @@
+SECTION "unused_gfx_picross_2", ROMX[$4000], BANK[$70]
+unused_gfx_picross_2::
+INCBIN "gfx/unused/picross_2.bin"
+.end::
+
 SECTION "gfx_lv_2_lake_zone", ROMX[$64c0], BANK[$70]
 gfx_lv_2_lake_zone::
 INCBIN "gfx/safari_map/lv_2_lake_zone.bin"
