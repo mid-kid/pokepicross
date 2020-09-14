dbb24fc1a9ab34b99166b269a240313e37932e43
Rangi <remy.oukaour+rangi42@gmail.com>
2020-09-14 17:05:03 -0400

    Extract more graphics

---
+++ source/bank_6d.asm
@@ -0,0 +1,4 @@
+SECTION "gfx_album_picture_frames_light", ROMX[$4000], BANK[$6d]
+gfx_album_picture_frames_light::
+INCBIN "gfx/safari_zone_album/picture_frames_light.bin"
+.end::
--- source/bank_79.asm
+++ source/bank_79.asm
@@ -1,3 +1,8 @@
+SECTION "gfx_cancel_ok_no_yes", ROMX[$4000], BANK[$79]
+gfx_cancel_ok_no_yes::
+INCBIN "gfx/menus/cancel_ok_no_yes.bin"
+.end::
+
 SECTION "compressed_tilemap_attrmap_town_map", ROMX[$5800], BANK[$79]
 compressed_tilemap_attrmap_town_map::
 INCBIN "gfx/town_map/town_map.tilemap_attrmap.xor"
