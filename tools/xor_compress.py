46b7c8acf4cdb06a2cbe72ecf4b7d51d74026c5c
Rangi <remy.oukaour+rangi42@gmail.com>
2020-09-14 13:47:20 -0400

    xor_compress.py and dump_decompress.py share variable names

--- tools/xor_compress.py
+++ tools/xor_compress.py
@@ -9,35 +9,35 @@ with (open(sys.argv[1], 'rb') if len(sys.argv) > 1 else sys.stdin) as f:
 
 n = len(data) 
 output = []
-v = 0
+v = 0x00
 i = 0
 
 while i < n:
-    val = data[i]
+    byte = data[i]
     i += 1
 
-    if data[i] == v: #>=0x80
+    if data[i] == v:
         # Alternating (>= 0x80)
         # Run stops at 0x81 bytes or when the values stop alternating
         size = 1
-        while i < n and size < 0x81 and data[i] == (v if size % 2 else val):
+        while i < n and size < 0x81 and data[i] == (v if size % 2 else byte):
             size += 1
             i += 1
         output.append(size + 0x7e)
-        output.append(v ^ val)
+        output.append(v ^ byte)
         if size % 2:
-            v = val
+            v = byte
 
     else:
         # Sequential (< 0x80)
         # Run stops at 0x80 bytes or when the value two ahead is equal to v
-        buffer = [v ^ val]
+        buffer = [v ^ byte]
         while i < n:
-            v = val
+            v = byte
             if len(buffer) > 0x7f or (i + 1 < n and data[i + 1] == v):
                 break
-            val = data[i]
-            buffer.append(v ^ val)
+            byte = data[i]
+            buffer.append(v ^ byte)
             i += 1
         output.append(len(buffer) - 1)
         output.extend(buffer)
