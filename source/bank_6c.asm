6bcb1f6dd4c578233702df10791f53c1b52193e3
Rangi <remy.oukaour+rangi42@gmail.com>
2020-09-19 14:55:43 -0400

    Separate two unknown graphics in bank 6C

--- source/bank_6c.asm
+++ source/bank_6c.asm
@@ -3,6 +3,11 @@ gfx_unknown_6c_4000::
 INCBIN "gfx/unknown/unknown_6c_4000.2bpp"
 .end::
 
+SECTION "gfx_unknown_6c_7000", ROMX[$7000], BANK[$6c]
+gfx_unknown_6c_7000::
+INCBIN "gfx/unknown/unknown_6c_7000.2bpp"
+.end::
+
 SECTION "compressed_tilemap_attrmap_pokedex_pic", ROMX[$7c00], BANK[$6c]
 compressed_tilemap_attrmap_pokedex_pic::
 INCBIN "gfx/pokedex/pokedex_pic.tilemap_attrmap.xor"
